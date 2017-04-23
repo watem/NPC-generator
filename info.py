2997b1a320c8bc586e915fab3e459fc9aae267df
watem <watem.main@gmail.com>
2017-04-22 22:36:22 -0400

    info.py update
    updating info.py with human names

---
+++ info.py
@@ -0,0 +1,110 @@
+# appearance     alignment  talent     mannerisms   interactions    bonds    "abilities"   flaws
+
+# -    name/race
+
+# half-elf directly refered to in app
+# alignments directly refered to in app
+# strengths and weaknesses directly refered to in app
+# sexes directly refered to in app
+
+# appearance
+appearance_list = ['Distinctive jewelry: earrings, necklace, circlet, bracelets, Piercings', 'Flamboyant or outlandish clothes', 'Formal, clean clothes', 'Ragged, dirty clothes', 'Pronounced scar', 'Missing teeth', 'Missing fingers', 'Unusual eye color (or two different colors)', 'Tattoos', 'Birthmark', 'Unusual skin color', 'Bald', 'Braided beard or hair', 'Unusual hair color', 'Nervous eye twitch', 'Distinctive nose', 'Distinctive posture (crooked or rigid)', 'Exceptionally beautiful','Exceptionally ugly']
+
+# alignment and ideal
+alignmentOrderList = ['Chaotic', 'Neutral', 'Lawful']
+alignmentMoralList = ['Good', 'Neutral', 'Evil']
+good = ['Beauty', 'Charity', 'Greater good', 'Life', 'Respect', 'Self-sacrifice']
+evil = ['Domination', 'Greed', 'Might',  'Pain', 'Retribution', 'Slaughter']
+lawful = ['Community', 'Fairness', 'Honor', 'Logic', 'Responsibility', 'Tradition']
+chaotic = ['Change', 'Creativity', 'Freedom', 'Independence', 'No limits', 'Whimsy']
+neutral = ['Balance', 'Knowledge', 'Live and let live', 'Moderation', 'Redemption', 'Self-knowledge']
+otherideal = ['Aspiration', 'Discovery', 'Glory', 'Nation', 'Neutrality', 'People']
+
+# talent
+talent_list = ["Plays a musical instrument", "Speaks several languages fluently", "Unbelievably lucky", "Perfect memory", "Great with animals", "Great with children", "Great at solving puzzles", "Great at one game", "Great at impersonations", "Draws beautifully", "Paints beautifully", "Sings beautifully", "Drinks everyone under the table", "Expert carpenter", "Expert cook", "Expert dart thrower and rock skipper", "Expert juggler", "Skilled actor and master of disguise", "Skilled dancer", "Knows thieves'cant"]
+
+
+# mannerisms
+mannerisms_list = ["Prone to singing, whistling, or humming quietly", "Speaks in rhyme or some other peculiar way", "Particularly low or high voice", "Slurs words, lisps, or stutters", "Enunciates overly clearly", "Speaks loudly", "Whispers", "Uses flowery speech or long words", "Frequently uses the wrong word", "Uses colorful oaths and exclamations", "Makes constant jokes or puns", "Prone to predictions of doom", "Fidgets", "Squints", "Stares into the distance", "Chews something", "Paces", "Taps fingers", "Bites fingernails", "Twirls hair or tugs beard"]
+
+# interactions
+interaction_list = ['Argumentative', 'Honest', 'Arrogant', 'Hot tempered', 'Blustering', 'Irritable', 'Rude', 'Ponderous', 'Curious', 'Quiet', 'Friendly', 'Suspicious']
+
+# bonds
+bond_list = ['Dedicated to fulfilling a personal life goal', 'Protective of close family members', 'Protective of colleagues or compatriots', 'Loyal to a benefactor, patron, or employer', 'Captivated by a romantic interest', 'Drawn to a special place', 'Protective of a sentimental keepsake', 'Protective of a valuable possession', 'Out for revenge', 10]
+
+# flaws and secrets
+flaw_list = ["Forbidden love or susceptibility to romance", "Enjoys decadent pleasures", "Arrogance", "Envies another creature's possessions or station", "Overpowering greed", "Prone to rage", "Has a powerful enemy", "Specific phobia", "Shameful or scandalous history", "Secret crime or misdeed", "Possession of forbidden lore", "Foolhardy bravery"]
+
+
+# race and name
+# racelist = ['dwarf', 'elf', 'halfling', 'human', 'dragonborn', 'gnome', 'half-elf', 'half-orc', 'tiefling']
+
+
+
+class dwarf:
+    name = 'Dwarf'
+    M = ['Adrik', 'Alberich', 'Baern', 'Barendd', 'Brottor', 'Bruenor', 'Dain', 'Darrak', 'Delg', 'Eberk', 'Einkil', 'Fargrim', 'Flint', 'Gardain', 'Harbek', 'Kildrak', 'Morgran', 'Orsik', 'Oskar', 'Rangrim', 'Rurik', 'Taklinn', 'Thoradin', 'Thorin', 'Tordek', 'Traubon', 'Travok', 'Ulfgar', 'Veit', 'Vondal']
+    F = ['Amber', 'Artin', 'Audhild', 'Bardryn', 'Dagnal', 'Diesa', 'Eldeth', 'Falkrunn', 'Finellen', 'Gunnloda', 'Gurdis', 'Helja', 'Hlin', 'Kathra', 'Kristryd', 'Ilde', 'Liftrasa', 'Mardred', 'Riswynn', 'Sannl', 'Torbera', 'Torgga',  'Vistra']
+    L = ['Balderk', 'Battlehammer', 'Brawnanvil', 'Dankil', 'Fireforge', 'Frostbeard', 'Gorunn', 'Holderhek', 'Ironfist', 'Loderr', 'Lutgehr', 'Rumnaheim', 'Strakeln', 'Torunn', 'Ungart']
+
+class elf:
+    name = 'Elf'
+    M = ['Ara(child name)', 'Bryn(child name)', 'Del(child name)', 'Eryn(child name)', 'Faen(child name)', 'Innil(child name) ', 'Lael(child name)', 'Mella(child name)', 'Naill(child name)', 'Naeris(child name)', 'Phann(child name)', 'Rael(child name)', 'Rinn(child name)', 'Sai(child name)', 'Syllin(child name)', 'Thia(child name)', 'Vall(child Name)', 'Adran', 'Aelar', 'Aramil', 'Arannis,Aust', 'Beiro', 'Berrian', 'Carric ', 'Enialis', 'Erdan', 'Erevan', 'Galinndan', 'Hadarai', 'Heian', 'Himo', 'Immeral', 'Ivellios', 'Laucian', 'Mindartis', 'Paelias', 'Peren', 'Quarion', 'Riardon', 'Rolen', 'Soveliss', 'Thamior', 'Tharivol', 'Theren', 'Varis']
+    F = ['Ara(child name)', 'Bryn(child name)', 'Del(child name)', 'Eryn(child name)', 'Faen(child name)', 'Innil(child name) ', 'Lael(child name)', 'Mella(child name)', 'Naill(child name)', 'Naeris(child name)', 'Phann(child name)', 'Rael(child name)', 'Rinn(child name)', 'Sai(child name)', 'Syllin(child name)', 'Thia(child name)', 'Vall(child Name)', 'Adrie', 'Althaea', 'Anastrianna', 'Andraste', 'Antinua', 'Bethrynna', 'Birel', 'Caelynn', 'Drusilia', 'Enna', 'Felosial', 'Ielenia', 'Jelenneth', 'Keyleth', 'Leshanna', 'Lia', 'Meriele', 'Mialee', 'Naivara', 'Quelenna', 'Quillathe', 'Sariel', 'Shanairra', 'Shava', 'Silaqui', 'Theirastra', 'Thia', 'Vadania', 'Valanthe', 'Xanaphia']
+    L = ['Amakiir (Gemflower)', 'Am astacia (Starflower)', 'Galanodel (Moonwhisper)', 'Holimion (Diamond dew)', 'Ilphelkiir (Gem blossom)', 'Liadon (Silverfrond)', 'Meliamne (Oakenheel)', 'Nai\'lo (Nightbreeze)', 'Siannodel (Moonbrook)', 'Xiloscient (Goldpetal)']
+
+class halfling:
+    name = 'Halfling'
+    M = ['Alton', 'Ander', 'Cade', 'Corrin', 'Eldon', 'Errich', 'Finnan', 'Garret', 'Lindal', 'Lyle', 'Merric', 'Milo', 'Osborn', 'Perrin', 'Reed', 'Roscoe', 'Wellby']
+    F = ['Andry', 'Bree', 'Callie', 'Cora', 'Euphemia', 'Jillian', 'Kithri', 'Lavinia', 'Lidda', 'Merla', 'Nedda', 'Paela', 'Portia', 'Seraphina', 'Shaena', 'Trym', 'Vani', 'Verna']
+    L = ['Brushgather', 'Goodbarrel', 'Greenbottle', 'High-hill', 'Hilltopple', 'Leagallow', 'Tealeaf', 'Thorngage', 'Tosscobble', 'Underbough']
+
+
+class human:
+    name = 'Human'
+    M = ["Aseir", "Bardeid", "Haseid,Khemed", "Mehmen", "Sudeiman", "Zasheir", "Darvin", "Dorn", "Evendur", "Gorstag", "Grim", "Helm", "Malark", "Morn", "Randal", "Stedd", "Bor", "Fodel", "Glar", "Grigor,Igan", "Ivor", "Kosef", "Mival", "Orel", "Pavel", "Sergor", "Ander", "Blath", "Bran", "Frath", "Geth", "Lander", "Luth", "Malcer", "Stor", "Taman", "Urth", "Aoth", "Bareris", "Ehput-Ki", "Kethoth", "Mumed", "Ramas", "So-Kehur", "Thazar-De", "Urhur", "Borivik", "Faurgar", "Jandar", "Kanithar", "Madislak", "Ralmevik", "Shaumar", "Vladislak", "An", "Chen", "Chi", "Fai", "Jiang", "Jun", "Lian", "Long", "Meng", "On", "Shan", "Shui", "Wen", "Anton", "Diero", "Marcon", "Pieron", "Rimardo", "Romero", "Salazar", "Umbero"]
+    F = ["Atala", "Ceidil", "Hama", "Jasmal", "Meilil", "Seipora", "Yasheira", "Zasheida", "Arveene", "Esvele", "Jhessail", "Kerri", "Lureene", "Miri", "Rowan", "Shandri", "Tessele", "Alethra", "Kara", "Katernin", "Mara", "Natali", "Olma", "Tana", "Zora", "Amafrey", "Betha", "Cefrey", "Kethra", "Mara", "Olga", "Silifrey", "Westra", "Arizima", "Chathi", "Nephis", "Nulara", "Murithi", "Sefris", "Thola", "Umara", "Zolis", "Fyevarra", "Hulmarra", "Immith", "Imzel", "Navarra", "Shevarra", "Tammith. Yuldra", "Bai", "Chao", "Jia", "Lei", "Mei", "Qiao", "Shui", "Tai", "Balama", "Dona", "Faila", "Jalana", "Luisa", "Marta", "Quara", "Selise", "Vonda"]
+    L = ["Basha", "Dumein", "Jassan", "Khalid", "Mostana", "Pashar", "Rein", "Amblecrown", "Buckman", "Dundragon", "Evenwood", "Greycastle", "Tallstag", "Bersk", "Chernin", "Dotsk", "Kulenov", "Marsk", "Nemetsk", "Shemov", "Starag", "Brightwood", "Helder", "Hornraven", "Lackman", "Stormwind", "Windrivver", "Ankhalab", "Anskuld", "Fezim", "Hahpet", "Nathandem", "Sepret", "Uuthrakt", "Chergoba", "Dyernina", "Iltazyara", "Murnyethara", "Stayanoga", "Ulmokina", "Chien", "Huang", "Kao", "Kung", "Lao", "Ling", "Mei", "Pin", "Shin", "Sum", "Tan", "Wan", "Agosto", "Astorio", "Calabra", "Domine", "Falone", "Marivaldi", "Pisacar", "Ramondo"]
+
+class dragonborn:
+    name = 'Dragonborn'
+    M = ['Arjhan', 'Balasar', 'Bharash', 'Donaar', 'Ghesh', 'Heskan', 'Kriv', 'Medrash', 'Mehen', 'Nadarr', 'Pandjed', 'Patrin', 'Rhogar', 'Shamash', 'Shedinn', 'Tarhun', 'Torinn', 'Climber(child name)', 'Earbender(child name)', 'Leaper(child name)', 'Pious(child name)', 'Shieldbiter(child name)', 'Zealous(child name)']
+    F = ['Akra ', 'Biri', 'Daar', 'Farideh', 'Harann', 'Flavilar', 'Jheri', 'Kava', 'Korinn', 'Mishann', 'Nala', 'Perra', 'Raiann', 'Sora', 'Surina', 'Thava', 'Uadjit', 'Climber(child name)', 'Earbender(child name)', 'Leaper(child name)', 'Pious(child name)', 'Shieldbiter(child name)', 'Zealous(child name)']
+    L = ['Clethtinthiallor', 'Daardendrian', 'Delmirev', 'Drachedandion', 'Fenkenkabradon', 'Kepeshkm olik', 'Kerrhylon', 'Kimbatuul', 'Linxakasendalor', 'Myastan', 'Nemmonis', 'Norixius', 'Ophinshtalajiir', 'Prexijandilin', 'Shestendeliath', 'Turnuroth', 'Verthisathurgiesh', 'Yarjerit']
+
+class gnome:
+    name = 'Gnome'
+    M = ['Alston', 'Alvyn', 'Boddynock', 'Brocc', 'Burgell', 'Dimble', 'Eldon', 'Erky', 'Fonkin', 'Frug', 'Gerbo', 'Gimble', 'Glim', 'Jebeddo', 'Kellen', 'Nam foodle', 'Orryn', 'Roondar', 'Seebo', 'Sindri', 'Warryn', 'Wrenn', 'Zook']
+    F = ['Bimpnottin', 'Breena', 'Caramip', 'Carlin', 'Donella', 'Duvamil', 'Ella', 'Ellyjobell', 'Ellywick', 'Lilli', 'Loopmottin', 'Lorilla', 'Mardnab', 'Nissa', 'Nyx', 'Oda', 'Orla', 'Roywyn', 'Shamil', 'Tana', 'Waywocket', 'Zanna']
+    L = ['Beren', 'Daergel', 'Folkor', 'Garrick', 'Nackle', 'Murnig', 'Ningel', 'Raulnor', 'Scheppen', 'Tim bers', 'Turen']
+    nick = ['Aleslosh', 'Ashhearth', 'Badger', 'Cloak', 'Doublelock', 'Filchbatter', 'Fnipper', 'Ku', 'Nim', 'Oneshoe', 'Pock', 'Sparklegem ', 'Stum bleduck']
+
+class half_elf:
+    name = 'Half-Elf'
+    # name references human and elf lists
+
+class half_orc:
+    name = 'Half-Orc'
+    M = ['Dench', 'Feng', 'Gell', 'Henk', 'Holg', 'Imsh', 'Keth', 'Krusk', 'Mhurren', 'Ront', 'Shump', 'Thokk']
+    F = ['Baggi', 'Emen', 'Engong', 'Kansif', 'Myev', 'Neega', 'Ovak', 'Ownka', 'Shautha', 'Sutha', 'Vola', 'Volen', 'Yevelda']
+    L = ['']
+
+class tiefling:
+    name = 'Tiefling'
+    M = ['Akmenos', 'Amnon', 'Barakas', 'Damakos', 'Ekemon', 'Iados', 'Kairon', 'Leucis', 'Melech', 'Mordai', 'Morthos', 'Pelaios', 'Skamos', 'Therai']
+    F = ['Akta', 'Anakis', 'Bryseis', 'Criella', 'Damaia', 'Ea', 'Kallista', 'Lerissa', 'Makaria', 'Nemeia', 'Orianna', 'Phelaia', 'Rieta']
+    L = ['Art', 'Carrion', 'Chant', 'Creed', 'Despair', 'Excellence', 'Fear', 'Glory', 'Hope', 'Ideal', 'Music', 'Nowhere', 'Open', 'Poetry', 'Quest', 'Random', 'Reverence', 'Sorrow', 'Temerity', 'Torment', 'Weary']
+
+
+racelist = {
+    'dwarf': dwarf,
+    'elf': elf,
+    'halfling': halfling,
+    'human': human,
+    'dragonborn': dragonborn,
+    'gnome': gnome,
+    'half-elf': half_elf,
+    'half-orc': half_orc,
+    'tiefling': tiefling
+}
